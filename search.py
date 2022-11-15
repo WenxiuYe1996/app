91c873c9e2f661bc707ebac0040ca47f9f63f8f8
Wenxiu Ye <56489872+WenxiuYe1996@users.noreply.github.com>
2022-11-14 20:59:25 -0500

    Add files via upload

---
+++ search.py
@@ -0,0 +1,15 @@
+
+def search(term, num_results=10, lang="en"):
+
+    #link_list =  getTextFromNasaWeb(term)
+    #doc_name, documents = get_doc_list('txt')
+    #lda_model_link = generateLDAmodel(doc_name, documents)
+
+    new_list = []
+    #for x in link_list:
+     #   new_list.append(x)
+
+    #for x in lda_model_link:
+     #   new_list.append(x)
+
+    return new_list
